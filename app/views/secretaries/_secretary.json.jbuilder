fb2e4288517cf74eeeaac44fd82082a32b0dcea4
Níckolas Emanuel <nickolas.silva@alunos.ufersa.edu.br>
2024-01-19 17:28:17 -0300

    add secretaries

---
+++ app/views/secretaries/_secretary.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! secretary, :id, :name, :created_at, :updated_at
+json.url secretary_url(secretary, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_01_18_193037) do
+ActiveRecord::Schema[7.1].define(version: 2024_01_19_202038) do
   create_table "admins", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -45,6 +45,12 @@ ActiveRecord::Schema[7.1].define(version: 2024_01_18_193037) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_employees_on_email", unique: true
     t.index ["reset_password_token"], name: "index_employees_on_reset_password_token", unique: true
+  end
+
+  create_table "secretaries", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
